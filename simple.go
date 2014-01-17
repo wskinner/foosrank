766cb98aae21612d3bb3f372ef0a6e1c7879120a
Will Skinner <meaningoflifeis42@gmail.com>
2014-01-16 21:06:54 -0800

    after modifying anaconda library, pulling mentions from twitter works

---
+++ simple.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/ChimeraCoder/anaconda"
+	"fmt"
+)
+
+func getApi() *anaconda.TwitterApi {
+	anaconda.SetConsumerKey("WyWJsRBoWWQvnG8jnoO0bA")
+	anaconda.SetConsumerSecret("1QxBiTHqcy5jeMgFzxwJyon5tdThphRzzOlmxiW3qG8")
+	var api *anaconda.TwitterApi = anaconda.NewTwitterApi("2295398977-RCw9S9GM5UZ2lkQC3prlNfAKWgnaUdoRdcBEh6k", "olpgzfjU4a47f67kJQh7TrZ5ZjvOVE1RLUaD0RpbyD4JH")
+
+	return api
+}
+
+func main() {
+
+	api := getApi()
+
+	mentions, _ := api.GetStatusesMentionsTimeline()
+	for _, tweet := range mentions {
+		fmt.Println(tweet.Text)
+	}
+}
