173527235a068ff83f91d7ed0277b10545b14f79
Michael Schiff <schiff.michael@gmail.com>
2014-01-18 15:59:52 -0800

    fixed testparser errors

--- testparser.go
+++ testparser.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	groups, err := foosrank.GetTweetEntities("will skinner 6 michael schiff 8")
-	winner, loser, err := foosrank.GetPlayers(groups)
+	winner, loser, s1, s2, err := foosrank.GetPlayers(groups)
 
-	fmt.Printf("winner: %+v\nloser: %+v\n", winner, loser)
+	fmt.Printf("winner: %+v, score: %d\nloser: %+v, score: %d\n", winner, s1, loser, s2)
 	fmt.Printf("err: %v\n", err)
 
 /*
