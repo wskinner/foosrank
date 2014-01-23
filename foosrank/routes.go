d7bc109b65da9792482dd12ae1d642c9394386c4
Ubuntu <ubuntu@ip-172-31-37-109.us-west-2.compute.internal>
2014-01-23 22:56:44 +0000

    changed routes.go to serve home.html as is (without templating).  Modified home.html so that the websocket host string does not need to be templated by the server.  Modified home.html so rank is not its own colum, instead it is a badge next to the players name

--- foosrank/routes.go
+++ foosrank/routes.go
@@ -20,9 +20,10 @@ func defaultAssetPath() string {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	homeTempl := template.Must(template.ParseFiles(filepath.Join(defaultAssetPath(), "home.html")))
+	//homeTempl := template.Must(template.ParseFiles(filepath.Join(defaultAssetPath(), "home.html")))
 	fmt.Println("Host: " + r.Host)
-	homeTempl.Execute(w, r.Host)
+	//homeTempl.Execute(w, r.Host)
+	http.ServeFile(w, r, filepath.Join(defaultAssetPath(), "home.html"))
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
